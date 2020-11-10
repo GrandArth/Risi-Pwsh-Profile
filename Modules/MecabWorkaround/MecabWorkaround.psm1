5308a8494f9cd9f2d78479695fe04d49b252ccfc
GrandARTH <v2629994284@163.com>
2020-11-10 09:48:00 +0800

    change to the easy way

--- Modules/MecabWorkaround/MecabWorkaround.psm1
+++ Modules/MecabWorkaround/MecabWorkaround.psm1
@@ -2,9 +2,12 @@ function mmecab{
 	param (
 		[string]$InputString
 	)
+	<#
 	Set-Content -LiteralPath "$($PWD)/temp.txt" -Value $InputString;
 	mecab "$($PWD)/temp.txt" -o "$($PWD)/temp-output.txt";
 	Get-Content -LiteralPath "$($PWD)/temp-output.txt";
 	Remove-Item -LiteralPath "$($PWD)/temp.txt";
 	Remove-Item -LiteralPath "$($PWD)/temp-output.txt";
+	#>
+	Write-Output $InputString | mecab ;
 }
