312eb5a35990f1058101dce12ea6c3d666cea20c
GrandARTH <v2629994284@163.com>
2020-11-19 14:42:21 +0800

    support dice experissions in modifer

--- Modules/RollDice/RollDice.psm1
+++ Modules/RollDice/RollDice.psm1
@@ -1,42 +1,44 @@
+function RollSingleDice{
+	param(
+		[Parameter(Position=0)][string]$DiceExperission="nd20"
+	)
+	$DiceArray = $DiceExperission.Split("d");
+	[int]$DiceGtZero = 1;
+	if($DiceArray[0] -eq "n"){
+		$DiceGtZero = -1;
+	}
+	if($DiceArray[1]){
+		$DiceMaxValue=[int]$DiceArray[1];
+		$ReVal = Get-Random  -Maximum $DiceMaxValue;
+		return $DiceGtZero*$ReVal;
+	}else {
+		return [int]$DiceArray[0]
+	}
+
+}
+
 function RollDice{
 	param(
-		[Parameter(Position=0)][string]$DiceExperission="1d20m0",
+		[Parameter(Position=0)][string]$DiceExperission="1d20md20m5mnd10",
 		[Parameter(Position=1)][string]$Executer
 	)
-	
-	[int]$DiceMultipler = 1;
-	[int]$DiceModifier = 0;
-	[int]$DiceMainRoll = 100;
 
-	$DiceArray = $DiceExperission.Split("d");
-	if($DiceArray[0]){
-		$DiceMultipler=[int]$DiceArray[0];
-	}
-	$DiceArrayBackHalf=$DiceArray[1].Split("m");
-	$DiceMainRoll=[int]$DiceArrayBackHalf[0];
-
-	[int]$WhileModiferIndex = 1;
-	while($DiceArrayBackHalf[$WhileModiferIndex]){
-		$DiceModifier=[int]$DiceModifier + [int]$DiceArrayBackHalf[$WhileModiferIndex];
-		$WhileModiferIndex = $WhileModiferIndex + 1
+	$RandomExperissions = $DiceExperission.Split("m");
+	$HeadExperissions = $RandomExperissions[0];
+	[int]$TotalNumber = $HeadExperissions.Split("d")[0];
+	if($TotalNumber -eq 0){
+		$TotalNumber = 1;
 	}
 
-	<#
-	if($DiceArrayBackHalf[1]){
-		$DiceModifier=[int]$DiceArrayBackHalf[1];
+	while($TotalNumber -ne 0){
+		$TotalNumber = $TotalNumber - 1;
+		$HeadValue = RollSingleDice $HeadExperissions;
+		[int]$ModifierIndexer = 1;
+		$TotalModifer = 0;
+		while($RandomExperissions[$ModifierIndexer]){
+			$TotalModifer = $TotalModifer + $(RollSingleDice $RandomExperissions[$ModifierIndexer]);
+			$ModifierIndexer = $ModifierIndexer + 1
+		}
+		Write-Output "$Executer roll dice $($TotalNumber) (Modifer $TotalModifer):    $($TotalModifer + $HeadValue)";
 	}
-	#>
-	$ReVal = Get-Random -Count $DiceMultipler -Maximum $DiceMainRoll;
-	$ReValState = $ReVal | Measure-Object -AllStats;
-	$VersusCount = 0;
-	$ReVal | ForEach-Object {
-		$DiceVal = $_ + $DiceModifier;
-		Write-Output "$Executer roll dice $($ReValState.Count - $VersusCount) (Modifer $DiceModifier):    $($DiceVal)";
-		$VersusCount = $VersusCount + 1;
-	}
-	if($ReValState.Count -gt 1){
-		Write-Output "Dice Result Sum: $($ReValState.Sum + $ReValState.Count * $DiceModifier)";
-	}
-
-
 }
