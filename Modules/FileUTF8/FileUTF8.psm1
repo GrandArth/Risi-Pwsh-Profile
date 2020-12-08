5ef2cb1baa42fc0fc40978e97d581aea8c3337d2
GrandARTH <v2629994284@163.com>
2020-12-08 10:31:19 +0800

    final

--- Modules/FileUTF8/FileUTF8.psm1
+++ Modules/FileUTF8/FileUTF8.psm1
@@ -12,11 +12,11 @@ function FileBRE{
 		$FileContent = Get-Content -LiteralPath $_.FullName -Raw -Encoding $OriginEncoder.CodePage;
 		if($FileContent){
 			if($TargetEncoding -eq "utf-8"){
-				Set-Content -LiteralPath "$($_.Directory)/$($_.BaseName)-$($TargetEncoding).$($FileExtension)" -Value $FileContent -Encoding "utf8NoBOM";
+				Set-Content -LiteralPath "$($_.Directory)/$($_.BaseName).$($TargetEncoding).$($FileExtension)" -Value $FileContent -Encoding "utf8NoBOM";
 			}else {
-				Set-Content -LiteralPath "$($_.Directory)/$($_.BaseName)-$($TargetEncoding).$($FileExtension)" -Value $FileContent -Encoding $TargetEncoder.CodePage;
+				Set-Content -LiteralPath "$($_.Directory)/$($_.BaseName).$($TargetEncoding).$($FileExtension)" -Value $FileContent -Encoding $TargetEncoder.CodePage;
 			}
 		}
-		Write-Output "$($_.Directory)/$($_.BaseName)-$($TargetEncoding).$($FileExtension)";
+		Write-Output "$($_.Directory)/$($_.BaseName).$($TargetEncoding).$($FileExtension)";
 	} 
 }
