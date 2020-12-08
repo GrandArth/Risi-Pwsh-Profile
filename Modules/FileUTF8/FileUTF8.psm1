05de9ce7d4f0de70e01da5a87d407d23b73626b8
GrandARTH <v2629994284@163.com>
2020-12-08 10:18:36 +0800

    remove cuebatch since we have fileutf8 module & fileutf8 module enhanced

--- Modules/FileUTF8/FileUTF8.psm1
+++ Modules/FileUTF8/FileUTF8.psm1
@@ -1,18 +1,22 @@
-function FileRE{
+function FileBRE{
 	param (
 		[Parameter(Position=0)][string]$OriginEncoding="GB2312",
-		[Parameter(Position=1,Mandatory=$true)][string]$FileExtension
+		[Parameter(Position=1,Mandatory=$true)][string]$FileExtension,
+		[Parameter(Position=2,Mandatory=$true)][string]$TargetEncoding="utf-8"
 	)
 	$OriginEncoder = [System.Text.Encoding]::GetEncoding($OriginEncoding);
-	$UTF8Encoder = [System.Text.Encoding]::GetEncoding("utf-8");
+	$TargetEncoder = [System.Text.Encoding]::GetEncoding($TargetEncoding);
 	Write-Output $OriginEncoder;
-	Write-Output $UTF8Encoder;
+	Write-Output $TargetEncoder;
 	Get-ChildItem  "*.$($FileExtension)" | ForEach-Object {
-		#$FileContent = Get-Content -LiteralPath "$($_.BaseName).cue" -Raw -AsByteStream;
-		$FileContent = Get-Content -LiteralPath $_.FullName -Raw -AsByteStream;
-		$FileContentConverted = [System.Text.Encoding]::Convert($OriginEncoder, $UTF8Encoder, $FileContent);
-		$FileString = $UTF8Encoder.GetString($FileContentConverted);
-		Set-Content -LiteralPath "$($_.Directory)/$($_.BaseName).$($FileExtension)" -Value $FileString;
-		Write-Output "$($_.Directory)/$($_.BaseName).$($FileExtension)";
+		$FileContent = Get-Content -LiteralPath $_.FullName -Raw -Encoding $OriginEncoder.CodePage;
+		if($FileContent){
+			if($TargetEncoding -eq "utf-8"){
+				Set-Content -LiteralPath "$($_.Directory)/$($_.BaseName)-$($TargetEncoding).$($FileExtension)" -Value $FileContent -Encoding "utf8NoBOM";
+			}else {
+				Set-Content -LiteralPath "$($_.Directory)/$($_.BaseName)-$($TargetEncoding).$($FileExtension)" -Value $FileContent -Encoding $TargetEncoder.CodePage;
+			}
+		}
+		Write-Output "$($_.Directory)/$($_.BaseName)-$($TargetEncoding).$($FileExtension)";
 	} 
 }
