87539c9565f1d92dee4105f92fb6735076f1b235
GrandArth <kousakiaya@icloud.com>
2022-03-28 11:48:48 +0800

    rewrite bilibili merge with different logic

--- Modules/BiliMerge/BiliMerge.psm1
+++ Modules/BiliMerge/BiliMerge.psm1
@@ -1,3 +1,12 @@
-function BMerge(){
-    Get-ChildItem *.txt | ForEach-Object{ffmpeg -safe 0 -f concat -i "$($_.FullName)" -c copy "$($_.BaseName).mp4"}
+function AudioVideo-Merge(){
+    $mp4Objects= ls *.mp4
+    $m4aObjects = ls *.m4a
+    ForEach($mp4Object in $mp4Objects){
+        ForEach($m4aObject in $m4aObjects){
+            if($mp4Object.BaseName -eq $m4aObject.BaseName){
+                ffmpeg -i $mp4Object.FullName -i $m4aObject.FullName -c:v copy -c:a copy "$($mp4Object.BaseName).muxed.mp4"
+            }
+        }
+    }
 }
+Set-Alias -Name av -Value AudioVideo-Merge;
