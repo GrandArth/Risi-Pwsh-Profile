8c0362326e61459f9b563e991e1451a66b0a6e40
GrandArth <kousakiaya@icloud.com>
2022-10-05 11:55:04 +0800

    add latex render cmd based on zhihu api

---
+++ Modules/LatexEqSVG/LatexEqSVG.psm1
@@ -0,0 +1,8 @@
+function Eq-Render{
+    param(
+        [Parameter(Position=0)][string]$EquationInString)
+    $urlEncoded_String = [System.Web.HTTPUtility]::UrlEncode($EquationInString)
+    $Zhihu_RenderUrl = "https://www.zhihu.com/equation?tex=$($urlEncoded_String)"
+    Set-Clipboard $Zhihu_RenderUrl
+    Write-Output $Zhihu_RenderUrl
+}
